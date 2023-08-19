48a8ce845c18b6d7e961ef238518e3eae8b741a5
Alex Cândido <alex.candido.tec@gmail.com>
2023-08-19 16:42:42 -0300

    adicionado estrutura de pastas de /components/...

---
+++ components/Elements/Form.vue
@@ -0,0 +1,3 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script lang="ts" setup></script>
+<template><div>**Form Component</div></template>
---
+++ components/Users/Bio.vue
@@ -0,0 +1,3 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script lang="ts" setup></script>
+<template><div>**UsersBio Component</div></template>

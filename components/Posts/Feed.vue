4b52cb5bc331b4efb020e1bcf8ced7f9297be5f6
Alex Cândido <alex.candido.tec@gmail.com>
2023-08-19 23:34:36 -0300

    adicionado estrutura de PostFeed em components/Posts/Feed.vue

--- components/Posts/Feed.vue
+++ components/Posts/Feed.vue
@@ -1,3 +1,26 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script lang="ts" setup></script>
-<template><div>**PostsFeed Component</div></template>
+<script lang="ts" setup>
+const props = defineProps({
+  userId: {
+    type: String,
+    default: '',
+  },
+})
+
+const posts = [
+  {
+    id: 'sdgaahbth68534r1sabr1',
+    body: 'Lorem ipsum',
+  },
+]
+</script>
+<template>
+  <div>
+    <PostItem
+      v-for="post in posts"
+      :key="post.id"
+      :user-id="props.userId"
+      :data="post"
+    />
+  </div>
+</template>

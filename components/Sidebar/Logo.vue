e969a99ba9298dba9e7fc1cae62cba994b9d0b6d
Alex Cândido <alex.candido.tec@gmail.com>
2023-08-19 22:05:03 -0300

    Adicionado estrutura SidebarLogo em components/Sidebar/Logo.vue

--- components/Sidebar/Logo.vue
+++ components/Sidebar/Logo.vue
@@ -1,3 +1,15 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script lang="ts" setup></script>
-<template><div>**SidebarLogo Component</div></template>
+<script setup lang="ts">
+const redirectToHome = () => {
+  const router = useRouter()
+  router.push('/')
+}
+</script>
+<template>
+  <div
+    class="rounded-full h-14 w-14 p-4 flex items-center justify-center hover:bg-opacity-10 cursor-pointer transition"
+    @click="redirectToHome"
+  >
+    <Icon name="brandico:twitter-bird" size="2rem" color="white" />
+  </div>
+</template>
